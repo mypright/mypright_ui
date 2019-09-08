73dc89e35f54d7c6156437f01cd813ea0147bd6f
buoyantair <buoyantair@protonmail.com>
2019-09-08 09:45:43 +0530

    resolve merge requests

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Button, Collapse, Icon, PageHeader, List, Avatar  } from 'antd';
-
+import React, { useState, useEffect, useRef } from 'react';
+import { Button,Collapse, Alert, Icon, PageHeader, Card, List, Avatar  } from 'antd';
 
 const {Panel} = Collapse;
 const text = `
@@ -23,51 +22,74 @@ const customPanelStyle = {
 };
 
 function ApprovalPage() {
-  const data = [
-    {
-      id: 2231,
-      siteUrl: 'https://google.com',
-      userDetail: [
-        {
-          detailName: 'Email Address',
-          required: true,
-          reason: text
-        },
-        {
-          detailName: "Aadhaar Number",
-          required: true,
-          reason: text
-        },
-        {
-          detailName: "Passport ID",
-          required: true,
-          reason: text
-        },
-      ]
-    },
-    {
-      id: 2232,
-      siteUrl: 'https://facebook.com',
-      userDetail: [
-        {
-          detailName: 'Your moms address',
-          required: true,
-          reason: text
-        },
-        {
-          detailName: "Your dads bank account numbers",
-          required: true,
-          reason: text
-        },
-        {
-          detailName: "Your secret boyfriend",
-          required: true,
-          reason: text
-        },
-      ]
-    },
-  ]
+  const API = 'https://myprightservice.herokuapp.com/site/data/all';
+  const [loadedState, setLoadedState] = useState(false);
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(API)
+      const json = await data.json()
+      return json
+    }
+    if (!loadedState) {
+      setData([...data, fetchData()])
+      setLoadedState(true)
+    }
+  }, [data, loadedState]);
+
+  const AccountList = (
+    <List
+      style={{margin: pageMargin}}
+      itemLayout="vertical"
+      dataSource={data}
+      renderItem={item => (
+        <Collapse
+            style={{background: 'none'}}
+            bordered={false}
+            defaultActiveKey={[]}
+            expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+          >
+            <Panel header={`${item.id} - ${item.siteUrl}`} style={customPanelStyle}>
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  title={<a href={item.siteUrl}>{item.siteUrl}</a>}
+                  description={`Uses lol`}
+                />
+                <h3
+                  style={{
+                    marginLeft: pageMargin * 2,
+                    marginRight: pageMargin * 2,
+                    marginTop: pageMargin,
+                    marginBottom: pageMargin
+                  }}
+                >
+                  Details
+                </h3>
+                <Collapse
+                  style={{background: 'none', margin: pageMargin}}
+                  bordered={false}
+                  defaultActiveKey={[]}
+                  expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
+                >
+                  {
+                    item.userDetail.map((detail, id) => (
+                      <Panel header={detail.detailName} key={id} style={{...customPanelStyle, background: '#f6f8ff'}}>
+                        <p>{detail.reason}</p>
+                      </Panel>
+                      
+                    ))
+                  }
+                </Collapse>
+              </List.Item>
+            </Panel>
+        </Collapse>
+      )}
+    />
+  )
+  
+  
   return (
     <section className="container">
       <PageHeader style={{margin: pageMargin, padding: 0}}>
@@ -75,64 +97,7 @@ function ApprovalPage() {
           Your data profile
         </h1>
       </PageHeader>
-      <List
-        style={{margin: pageMargin}}
-        itemLayout="vertical"
-        dataSource={data}
-        renderItem={item => (
-          <Collapse
-              style={{background: 'none'}}
-              bordered={false}
-              defaultActiveKey={[]}
-              expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
-            >
-              <Panel header={`${item.id} - ${item.siteUrl}`} style={customPanelStyle}>
-                <List.Item>
-                  <List.Item.Meta
-                    avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-                    title={<a href={item.siteUrl}>{item.siteUrl}</a>}
-                    description={`Uses lol`}
-                  />
-                  <h3
-                    style={{
-                      marginLeft: pageMargin * 2,
-                      marginRight: pageMargin * 2,
-                      marginTop: pageMargin,
-                      marginBottom: pageMargin
-                    }}
-                  >
-                    Details
-                  </h3>
-                  <Collapse
-                    style={{background: 'none', margin: pageMargin}}
-                    bordered={false}
-                    defaultActiveKey={[]}
-                    expandIcon={({ isActive }) => <Icon type="caret-right" rotate={isActive ? 90 : 0} />}
-                  >
-                    {
-                      item.userDetail.map((detail, id) => (
-                        <Panel header={detail.detailName} key={id} style={{...customPanelStyle, background: '#f6f8ff'}}>
-                          <p>{detail.reason}</p>
-                        </Panel>
-
-                      ))
-                    }
-                  </Collapse>
-                </List.Item>
-              </Panel>
-          </Collapse>
-        )}
-      />
-
-      <Button.Group style={{
-        display: 'flex',
-        marginTop: pageMargin,
-        marginBottom: pageMargin,
-        flexFlow: 'row wrap',
-        justifyContent: 'center'
-      }}>
-        <Button type="primary" size="large">Approve Request</Button>
-      </Button.Group>
+      {loadedState ? AccountList : 'Loading...'}
     </section>
   );
 }
